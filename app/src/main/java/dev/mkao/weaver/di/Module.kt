516cec5bcdd9655b8b19225484eaf44f6a074417
mkao <mkaoibrahim@gmail.com>
2023-08-12 00:23:31 +0300

    dependecy injection lib added

--- app/src/main/java/dev/mkao/weaver/di/Module.kt
+++ app/src/main/java/dev/mkao/weaver/di/Module.kt
@@ -1,5 +1,30 @@
 package dev.mkao.weaver.di
 
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import dev.mkao.weaver.data.remote.NewsApi
+import dev.mkao.weaver.data.remote.NewsApi.Companion.BASE_URL
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
 
-object Module  {
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+	
+	@Provides
+	@Singleton
+	fun provideNewsApi(): NewsApi {
+		val retrofit = Retrofit.Builder()
+			.baseUrl(BASE_URL)
+			.addConverterFactory(GsonConverterFactory.create())
+			.build()
+		return retrofit.create(NewsApi::class.java)
+	}
 }
+
+
+
