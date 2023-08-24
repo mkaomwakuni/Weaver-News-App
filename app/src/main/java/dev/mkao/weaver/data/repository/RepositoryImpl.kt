094c6965e4da94dc504ee9c3e1e0e93cd15e9934
mkao <mkaoibrahim@gmail.com>
2023-08-25 00:54:58 +0300

    Fixed a Typo

--- app/src/main/java/dev/mkao/weaver/data/repository/RepositoryImpl.kt
+++ app/src/main/java/dev/mkao/weaver/data/repository/RepositoryImpl.kt
@@ -1,19 +1,30 @@
-package dev.mkao.weaver.repository
+package dev.mkao.weaver.data.repository
 
 import dev.mkao.weaver.data.remote.NewsApi
 import dev.mkao.weaver.domain.model.Article
-import dev.mkao.weaver.domain.model.repository.Repository
+import dev.mkao.weaver.domain.repository.Repository
 import dev.mkao.weaver.util.Assets
 
-class RepositoryImpl (private val newsApi: NewsApi) : Repository {
+
+class RepositoryImpl(
+	private val newsApi: NewsApi
+): Repository {
 	
 	override suspend fun getTopHeadlines(category: String): Assets<List<Article>> {
-	
-		return  try {
+		return try {
 			val response = newsApi.getTopHeadlines(category = category)
-				Assets.SuccessResponse(response.article)
-			}catch (e:Exception){
-				Assets.ErrorResponse(message = "Oops Unable Fetch! ${e.message}")
+			Assets.Success(data = response.articles)
+		} catch (e: Exception) {
+			Assets.Error(message = "Failed to fetch news ${e.message}")
+		}
+	}
+	
+	override suspend fun searchForNews(query: String): Assets<List<Article>> {
+		return try {
+			val response = newsApi.searchForNews(query = query)
+			Assets.Success(data = response.articles)
+		} catch (e: Exception) {
+			Assets.Error(message = "Failed to fetch news ${e.message}")
 		}
 	}
 }
