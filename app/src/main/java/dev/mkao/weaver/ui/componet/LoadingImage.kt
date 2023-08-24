1de380fdde9cb41065708113e1a10beb487de0cc
mkao <mkaoibrahim@gmail.com>
2023-08-24 11:46:58 +0300

    Image-holder for loading image effect

--- app/src/main/java/dev/mkao/weaver/ui/componet/LoadingImage.kt
+++ app/src/main/java/dev/mkao/weaver/ui/componet/LoadingImage.kt
@@ -1,8 +1,37 @@
 package dev.mkao.weaver.ui.componet
 
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import dev.mkao.weaver.R
 
 @Composable
-fun LoadingImage (){
+fun LoadingImage (
+	imageUrl: String?,
+	modifier: Modifier = Modifier
+){
+AsyncImage(model = ImageRequest
+	.Builder(LocalContext.current)
+	.data(imageUrl)
+	.crossfade(true)
+	.build(),
+	contentScale = ContentScale.Crop,
+	contentDescription = "Loading Image",
+	modifier = Modifier
+		.clip(RoundedCornerShape(4.dp))
+		.fillMaxSize()
+		.aspectRatio(15 / 8f),
+    placeholder = painterResource(R.drawable.ic_launcher_foreground),
+	error = painterResource(id = coil.base.R.drawable.abc_vector_test)
+)
 
 }
