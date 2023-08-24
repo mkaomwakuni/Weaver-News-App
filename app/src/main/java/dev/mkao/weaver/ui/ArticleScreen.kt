02fee532cc333b7aa5758c3270d294e19f7ad713
mkao <mkaoibrahim@gmail.com>
2023-08-24 11:31:45 +0300

    Created a Card for news article

--- app/src/main/java/dev/mkao/weaver/ui/ArticleScreen.kt
+++ app/src/main/java/dev/mkao/weaver/ui/ArticleScreen.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -18,7 +17,7 @@ fun ArticleScreen ( viewModel: ArticleScreenViewModel = hiltViewModel()) {
 	contentPadding = PaddingValues(10.dp)
 	){
 		items(viewModel.newsArticles){ newsArticle ->
-			Text(text = newsArticle.title)
+			ArticleCard(article = newsArticle, onClickingCard = {})
 		}
 	}
 }
