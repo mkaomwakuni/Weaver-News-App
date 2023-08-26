7ba4ccfe0db9fc0ed5a3ad5a4da5bb07f8f63783
mkao <mkaoibrahim@gmail.com>
2023-08-27 01:04:36 +0300

    Tracing Responses to viewmodel

--- app/src/main/java/dev/mkao/weaver/viewModel/ArticleScreenViewModel.kt
+++ app/src/main/java/dev/mkao/weaver/viewModel/ArticleScreenViewModel.kt
@@ -7,25 +7,28 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import dev.mkao.weaver.domain.model.Article
-import dev.mkao.weaver.domain.model.repository.Repository
+import dev.mkao.weaver.domain.repository.Repository
 import dev.mkao.weaver.util.Assets
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class ArticleScreenViewModel @Inject constructor (
-	private val repository: Repository) : ViewModel() {
+class ArticleScreenViewModel @Inject constructor(
+	private val repository: Repository
+) : ViewModel() {
 	
 	var newsArticles by mutableStateOf<List<Article>>(emptyList())
 	
-	private  fun getNewsArticle(category :String){
+	fun getNewsArticles(category: String) {
 		viewModelScope.launch {
-			val result = repository.getTopHeadlines(category = category)
-			when (result){
-				is  Assets.SuccessResponse -> {
+			val result = repository.getTopHeadlines(category)
+			when (result) {
+				is Assets.Success -> {
 					newsArticles = result.data ?: emptyList()
 				}
-				is Assets.ErrorResponse -> TODO()
+				is Assets.Error -> {
+				
+				}
 			}
 		}
 	}
