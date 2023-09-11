09524ad68c5b52c3f6c4c3496ef0f0c73779d83a
mkao <mkaoibrahim@gmail.com>
2023-09-11 11:07:05 +0300

    Bottom sheet and error handling

---
+++ app/src/main/java/dev/mkao/weaver/ui/componet/ErrorHandler.kt
@@ -0,0 +1,34 @@
+package dev.mkao.weaver.ui.componet
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+@Composable
+fun ErrorHandler(
+	error: String,
+	onReload: () -> Unit,
+	modifier: Modifier = Modifier
+) {
+	Column (
+		modifier = Modifier
+	) {
+		Text(
+				text = error,
+				color = Color.Red,
+				fontSize = 18.sp
+		)
+			Spacer(modifier = Modifier.height(8.dp))
+			Button(onClick = onReload,modifier = Modifier.align(CenterHorizontally)) {
+			Text(text = "Reload")
+		}
+		
+	}
+}
