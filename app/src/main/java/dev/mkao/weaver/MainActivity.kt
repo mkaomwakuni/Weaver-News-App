e31d33665d55184a9bdf91384af3ec8a7beeee34
mkaocodes <mkaoibrahim@gmail.com>
2023-10-14 12:41:33 +0300

    Dark-mode Support for splashscreen

--- app/src/main/java/dev/mkao/weaver/MainActivity.kt
+++ app/src/main/java/dev/mkao/weaver/MainActivity.kt
@@ -3,6 +3,7 @@ package dev.mkao.weaver
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
 import dev.mkao.weaver.ui.theme.WeaverTheme
@@ -12,6 +13,7 @@ import dev.mkao.weaver.util.NewsNavGraph
 class MainActivity : ComponentActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
+		installSplashScreen()
 		setContent {
 			WeaverTheme {
 				val navController = rememberNavController()
