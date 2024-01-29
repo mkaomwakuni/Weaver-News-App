6e3cbb6ba44d91ab26206fc203297cb04b6f1eec
mkaocodes <mkaoibrahim@gmail.com>
2024-01-29 16:09:08 +0300

    Minor adjustments on state

--- app/src/main/java/dev/mkao/weaver/ui/componet/BottomDialog.kt
+++ app/src/main/java/dev/mkao/weaver/ui/componet/BottomDialog.kt
@@ -1,4 +1,4 @@
-
+package dev.mkao.weaver.ui.componet
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -12,6 +12,10 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
@@ -20,7 +24,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import dev.mkao.weaver.domain.model.Article
 import dev.mkao.weaver.domain.model.Source
-import dev.mkao.weaver.ui.componet.LoadingImage
+import dev.mkao.weaver.ui.article.NewsArticleUi
 
 
 @Composable
@@ -29,53 +33,66 @@ fun BottomDialog(
 	onReadFullStoryButtonClicked: () -> Unit
 ) {
 	val dynamicColors = MaterialTheme.colorScheme
-	Surface(
-		modifier = Modifier.fillMaxWidth(),
-		color = dynamicColors.primaryContainer
-	) {
-		Column(modifier = Modifier
-			.padding(16.dp)
-			.background(dynamicColors.primaryContainer) // Ensure the background color
-			.fillMaxWidth()
-			.padding(bottom = 16.dp),
-			horizontalAlignment = Alignment.CenterHorizontally) {
-			Text(
-				text = article.title,
-				style = MaterialTheme.typography.titleMedium,
-				fontWeight = FontWeight.Bold
-			)
-			Spacer(modifier = Modifier.height(8.dp))
-			LoadingImage(imageUrl = article.urlToImage)
-			Spacer(modifier = Modifier.height(8.dp))
-			Text(
-				text = article.content?.take(550)?:"none",
-				maxLines = 5,
-				overflow = TextOverflow.Ellipsis,
-				style = MaterialTheme.typography.bodyMedium
+	var showNewsArticleUi by remember { mutableStateOf(false) }
 
-			)
-			Spacer(modifier = Modifier.height(8.dp))
-			Row(
-				modifier = Modifier.fillMaxWidth(),
-				horizontalArrangement = Arrangement.SpaceBetween
+	if (showNewsArticleUi) {
+		NewsArticleUi(
+			url = article.url,
+			onBackPressed = { showNewsArticleUi = false }
+		)
+	} else {
+		Surface(
+			modifier = Modifier.fillMaxWidth(),
+			color = dynamicColors.primaryContainer
+		) {
+			Column(
+				modifier = Modifier
+					.padding(16.dp)
+					.background(dynamicColors.primaryContainer)
+					.fillMaxWidth()
+					.padding(bottom = 16.dp),
+				horizontalAlignment = Alignment.CenterHorizontally
 			) {
 				Text(
-					text = article.author?: "Source",
-					style = MaterialTheme.typography.bodySmall,
+					text = article.title,
+					style = MaterialTheme.typography.titleMedium,
 					fontWeight = FontWeight.Bold
 				)
+				Spacer(modifier = Modifier.height(8.dp))
+				LoadingImage(imageUrl = article.urlToImage)
+				Spacer(modifier = Modifier.height(8.dp))
 				Text(
-					text = article.source.name?: "Google News",
-					style = MaterialTheme.typography.bodySmall,
-					fontWeight = FontWeight.Bold
+					text = article.content?.take(550) ?: "none",
+					maxLines = 5,
+					overflow = TextOverflow.Ellipsis,
+					style = MaterialTheme.typography.bodyMedium
 				)
-			}
-			Spacer(modifier = Modifier.height(8.dp))
-			Button(
-				modifier = Modifier.fillMaxWidth(),
-				onClick = onReadFullStoryButtonClicked
-			) {
-				Text(text = "Read Full Story")
+				Spacer(modifier = Modifier.height(8.dp))
+				Row(
+					modifier = Modifier.fillMaxWidth(),
+					horizontalArrangement = Arrangement.SpaceBetween
+				) {
+					Text(
+						text = article.author ?: "Source",
+						style = MaterialTheme.typography.bodySmall,
+						fontWeight = FontWeight.Bold
+					)
+					Text(
+						text = article.source.name ?: "Google News",
+						style = MaterialTheme.typography.bodySmall,
+						fontWeight = FontWeight.Bold
+					)
+				}
+				Spacer(modifier = Modifier.height(8.dp))
+				Button(
+					modifier = Modifier.fillMaxWidth(),
+					onClick = {
+						onReadFullStoryButtonClicked()
+						showNewsArticleUi = true
+					}
+				) {
+					Text(text = "Read Full Story")
+				}
 			}
 		}
 	}
