efebfec716a13bb23ba10fb750ce8f616ec8506e
mkaocodes <mkaoibrahim@gmail.com>
2024-03-16 23:40:36 +0300

    Fixed Dynamic Color redundancy

--- app/src/main/java/dev/mkao/weaver/presentation/ui/theme/Theme.kt
+++ app/src/main/java/dev/mkao/weaver/presentation/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package dev.mkao.weaver.presentation.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -11,10 +10,7 @@ import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalView
-import androidx.core.view.WindowCompat
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 private val DarkColorScheme = darkColorScheme(
@@ -42,7 +38,6 @@ private val LightColorScheme = lightColorScheme(
 @Composable
 fun WeaverTheme(
 	darkTheme: Boolean = isSystemInDarkTheme(),
-	// Dynamic color is available on Android 12+
 	dynamicColor: Boolean = true,
 	content: @Composable () -> Unit
 ) {
@@ -51,17 +46,8 @@ fun WeaverTheme(
 			val context = LocalContext.current
 			if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
 		}
-
 		darkTheme -> DarkColorScheme
 		else -> LightColorScheme
-	}
-	val view = LocalView.current
-	if (!view.isInEditMode) {
-		SideEffect {
-			val window = (view.context as Activity).window
-			window.statusBarColor = colorScheme.primary.toArgb()
-			WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
-		}
 	}
 
 	val systemUiController = rememberSystemUiController()
