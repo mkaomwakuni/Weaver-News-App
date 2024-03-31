1992006fc9b0e189f0aa57978dcc382a7cbe0b0a
mkaocodes <mkaoibrahim@gmail.com>
2024-04-01 00:19:46 +0300

    Added a missing Library

--- app/src/main/java/dev/mkao/weaver/presentation/ui/theme/Theme.kt
+++ app/src/main/java/dev/mkao/weaver/presentation/ui/theme/Theme.kt
@@ -13,6 +13,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
+
 private val DarkColorScheme = darkColorScheme(
 	primary = Purple80,
 	secondary = PurpleGrey80,
