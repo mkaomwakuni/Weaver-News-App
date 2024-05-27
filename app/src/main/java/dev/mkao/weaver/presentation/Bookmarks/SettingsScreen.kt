5765eaff557c45519749b95323ec3765416172c2
mkaocodes <mkaoibrahim@gmail.com>
2024-05-27 22:58:24 +0300

    Added a settings interface

--- app/src/main/java/dev/mkao/weaver/presentation/Bookmarks/SettingsScreen.kt
+++ app/src/main/java/dev/mkao/weaver/presentation/Bookmarks/SettingsScreen.kt
@@ -1,23 +1,37 @@
-package dev.mkao.weaver.presentation.settings
+package dev.mkao.weaver.presentation.Bookmarks
 
 import android.annotation.SuppressLint
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowForward
-import androidx.compose.material.icons.filled.ArrowForward
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.LocalTextStyle
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Switch
+import androidx.compose.material3.SwitchDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.TextFieldValue
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -54,7 +68,7 @@ fun SettingsScreen() {
                         .padding(16.dp)
                 )
             }
-            Divider(color = Color(0xFFFFE4B5), thickness = 1.dp)
+            HorizontalDivider(thickness = 1.dp, color = Color(0xFFFFE4B5))
             SettingsCard(title = "Enable Notifications", trailing = {
                 Switch(
                     checked = notificationEnabled,
@@ -62,7 +76,7 @@ fun SettingsScreen() {
                     colors = SwitchDefaults.colors(checkedThumbColor = Color.LightGray)
                 )
             })
-            Divider(color = Color(0xFFFFE4B5), thickness = 1.dp)
+            HorizontalDivider(thickness = 1.dp, color = Color(0xFFFFE4B5))
             SettingsCard(title = "Enable Dark Mode", trailing = {
                 Switch(
                     checked = darkModeEnabled,
@@ -73,14 +87,14 @@ fun SettingsScreen() {
 
             SectionTitle(title = "General")
             SettingsCard(title = "Invite Friends")
-            Divider(color = Color(0xFFFFE4B5), thickness = 1.dp)
+            HorizontalDivider(thickness = 1.dp, color = Color(0xFFFFE4B5))
             SettingsCard(title = "Share on Instagram")
-            Divider(color = Color(0xFFFFE4B5), thickness = 1.dp)
+            HorizontalDivider(thickness = 1.dp, color = Color(0xFFFFE4B5))
             SettingsCard(title = "Share on TikTok")
 
             SectionTitle(title = "Other")
             SettingsCard(title = "Manage Consent")
-            Divider(color = Color(0xFFFFE4B5), thickness = 1.dp)
+            HorizontalDivider(thickness = 1.dp, color = Color(0xFFFFE4B5))
             SettingsCard(title = "About App")
         }
     }
