470967c200649c684cb696c8ae1c1544099ebf6a
mkaocodes <mkaoibrahim@gmail.com>
2024-05-28 19:17:32 +0300

    Icons updated

--- app/src/main/java/dev/mkao/weaver/presentation/Bookmarks/BottomNavigationBar.kt
+++ app/src/main/java/dev/mkao/weaver/presentation/Bookmarks/BottomNavigationBar.kt
@@ -1,6 +1,7 @@
 package dev.mkao.weaver.presentation.navgraph
 
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.CheckCircle
 import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Settings
@@ -10,10 +11,13 @@ import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import dev.mkao.weaver.R
 
 @Composable
 fun BottomNavigationBar(navController: NavController) {
@@ -22,8 +26,8 @@ fun BottomNavigationBar(navController: NavController) {
 
     NavigationBar {
         NavigationBarItem(
-            icon = { Icon(imageVector = Icons.Default.Home, contentDescription = "Home") },
-            label = { Text("Home") },
+            icon = { Icon(painter = painterResource(id = R.drawable.dashboard), contentDescription = "Home") },
+            label = { Text(stringResource(R.string.dashboard)) },
             selected = currentRoute == "article_screen",
             onClick = {
                 navController.navigate("article_screen") {
@@ -34,8 +38,20 @@ fun BottomNavigationBar(navController: NavController) {
             }
         )
         NavigationBarItem(
-            icon = { Icon(imageVector = Icons.Default.FavoriteBorder, contentDescription = "Bookmarks") },
-            label = { Text("Bookmarks") },
+            icon = { Icon(imageVector = Icons.Default.CheckCircle, contentDescription = "Home") },
+            label = { Text(stringResource(R.string.bulletin)) },
+            selected = currentRoute == "article_screen",
+            onClick = {
+                navController.navigate("article_screen") {
+                    popUpTo(navController.graph.startDestinationId) { saveState = true }
+                    launchSingleTop = true
+                    restoreState = true
+                }
+            }
+        )
+        NavigationBarItem(
+            icon = { Icon(painter = painterResource(id = R.drawable.bookmark), contentDescription = "Bookmarks") },
+            label = { Text(stringResource(R.string.bookmarks)) },
             selected = currentRoute == "bookmarks_screen",
             onClick = {
                 navController.navigate("bookmarks_screen") {
@@ -47,7 +63,7 @@ fun BottomNavigationBar(navController: NavController) {
         )
         NavigationBarItem(
             icon = { Icon(imageVector = Icons.Default.Settings, contentDescription = "Settings") },
-            label = { Text("Settings") },
+            label = { Text(stringResource(R.string.settings)) },
             selected = currentRoute == "settings_screen",
             onClick = {
                 navController.navigate("settings_screen") {
