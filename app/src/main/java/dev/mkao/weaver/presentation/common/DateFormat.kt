2b4bd24af75e9ae23345dd959dbe79e20e18c482
mkaocodes <mkaoibrahim@gmail.com>
2024-06-06 22:57:24 +0300

    -Added a Elapsed time for an article

--- app/src/main/java/dev/mkao/weaver/presentation/common/DateFormat.kt
+++ app/src/main/java/dev/mkao/weaver/presentation/common/DateFormat.kt
@@ -1,5 +1,6 @@
 package dev.mkao.weaver.presentation.common
 
+import java.time.Duration
 import java.time.OffsetDateTime
 import java.time.format.DateTimeFormatter
 import java.time.format.FormatStyle
@@ -17,4 +18,20 @@ fun dateFormat(inputDateTime: String?):String {
         ""
     }
     return dateString
+}
+fun calculateElapsedTime(publishedAt: String?): String {
+    return try {
+        val publishedDateTime = OffsetDateTime.parse(publishedAt, DateTimeFormatter.ISO_OFFSET_DATE_TIME)
+        val currentDateTime = OffsetDateTime.now()
+        val duration = Duration.between(publishedDateTime, currentDateTime)
+        when {
+            duration.toDays() > 0 -> "${duration.toDays()} day ago"
+            duration.toHours() > 0 -> "${duration.toHours()} hours ago"
+            duration.toMinutes() > 0 -> "${duration.toMinutes()} minutes ago"
+            else -> "Just now"
+        }
+    } catch (e: Exception) {
+        e.printStackTrace()
+        "Unknown time"
+    }
 }
