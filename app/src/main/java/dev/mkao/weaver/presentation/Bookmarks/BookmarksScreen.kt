352587832332879c4c4c889fa95c8f5edbbeab98
mkaocodes <mkaoibrahim@gmail.com>
2024-06-18 23:42:27 +0300

    - Minor UI Changes

--- app/src/main/java/dev/mkao/weaver/presentation/Bookmarks/BookmarksScreen.kt
+++ app/src/main/java/dev/mkao/weaver/presentation/Bookmarks/BookmarksScreen.kt
@@ -1,19 +1,28 @@
-package dev.mkao.weaver.presentation.bookmarks
+package dev.mkao.weaver.presentation.Bookmarks
 
 import android.annotation.SuppressLint
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material3.Card
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -129,4 +138,4 @@ fun BookmarksScreenPreview() {
 }
 
 @Composable
-fun Source(id: String, name: String) = Source(id, name)
+fun Source(id: String, name: String) = Source(id, name, category ="", url = "")
