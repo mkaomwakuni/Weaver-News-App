1f885409f7d3179a11582b3df1f92716f0926ae7
mkaocodes <mkaoibrahim@gmail.com>
2024-07-02 12:25:46 +0300

    - Resources updated/Refactoring

--- app/src/main/java/dev/mkao/weaver/util/Dimens.kt
+++ app/src/main/java/dev/mkao/weaver/util/Dimens.kt
@@ -5,6 +5,7 @@ import androidx.compose.ui.unit.dp
 object Dimens {
     val ArticleCard = 124.dp
     val ExtraSmallPadding = 3.dp
-    val ExtraSmallPadding1 = 6.dp
+    val ExtraSmallPadding2 = 6.dp
+    val ExtraExtraPadding = 15.dp
     val MediumPadding1 = 24.dp
 }
