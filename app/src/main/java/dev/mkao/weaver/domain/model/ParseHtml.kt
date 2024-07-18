0427f5cb11c1dcc902947eb23b4b7f225d23bff9
mkaocodes <mkaoibrahim@gmail.com>
2024-07-18 20:10:17 +0300

    Remove API Key
    - Removed Unsafe requests from the code for security reasons.

--- app/src/main/java/dev/mkao/weaver/domain/model/ParseHtml.kt
+++ app/src/main/java/dev/mkao/weaver/domain/model/ParseHtml.kt
@@ -8,19 +8,19 @@ import okhttp3.Request
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Element
 import java.io.IOException
-import java.security.cert.X509Certificate
+import java.security.KeyStore
 import java.util.concurrent.TimeUnit
 import javax.net.ssl.SSLContext
-import javax.net.ssl.TrustManager
+import javax.net.ssl.TrustManagerFactory
 import javax.net.ssl.X509TrustManager
 
+
 suspend fun fetchFullArticleContent(url: String): String = withContext(Dispatchers.IO) {
-    val client = getUnsafeOkHttpClient()
-    val httpsUrl = url.replace("http://", "https://")
-    val request = Request.Builder().url(httpsUrl).build()
+    val client = getSecureOkHttpClient()
+    val request = Request.Builder().url(url).build()
 
     try {
-        withTimeout(9000L) { // 5 second timeout
+        withTimeout(9000L) { // 9 second timeout
             val response = client.newCall(request).execute()
             val html = response.body?.string() ?: ""
             val doc = Jsoup.parse(html)
@@ -62,19 +62,16 @@ suspend fun fetchFullArticleContent(url: String): String = withContext(Dispatche
     }
 }
 
-private fun getUnsafeOkHttpClient(): OkHttpClient {
-    val trustAllCerts = arrayOf<TrustManager>(object : X509TrustManager {
-        override fun checkClientTrusted(chain: Array<out X509Certificate>?, authType: String?) {}
-        override fun checkServerTrusted(chain: Array<out X509Certificate>?, authType: String?) {}
-        override fun getAcceptedIssuers() = arrayOf<X509Certificate>()
-    })
+private fun getSecureOkHttpClient(): OkHttpClient {
+    val trustManagerFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm())
+    trustManagerFactory.init(null as KeyStore?)
+    val trustManagers = trustManagerFactory.trustManagers
 
-    val sslContext = SSLContext.getInstance("SSL")
-    sslContext.init(null, trustAllCerts, java.security.SecureRandom())
+    val sslContext = SSLContext.getInstance("TLS")
+    sslContext.init(null, trustManagers, null)
 
     return OkHttpClient.Builder()
-        .sslSocketFactory(sslContext.socketFactory, trustAllCerts[0] as X509TrustManager)
-        .hostnameVerifier { _, _ -> true }
+        .sslSocketFactory(sslContext.socketFactory, trustManagers[0] as X509TrustManager)
         .connectTimeout(6, TimeUnit.SECONDS)
         .readTimeout(6, TimeUnit.SECONDS)
         .build()
