7d1c1548eaa65e28f568a35f198561a83e7f9e1e
mkaocodes <mkaoibrahim@gmail.com>
2024-08-06 20:29:35 +0300

    - Refactor: Improved UI - Improved UI of the Bookmarks screen. - Updated the UI  to display the article elements. - Updated the UI to display the elapsed time since the article was published.

--- app/src/main/java/dev/mkao/weaver/presentation/bookmarks/BookmarksScreen.kt
+++ app/src/main/java/dev/mkao/weaver/presentation/bookmarks/BookmarksScreen.kt
@@ -1,16 +1,18 @@
-package dev.mkao.weaver.presentation.Bookmarks
+package dev.mkao.weaver.presentation.bookmarks
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.CenterAlignedTopAppBar
@@ -23,11 +25,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -53,51 +56,73 @@ fun ArticleList(articles: List<Article>, sharedViewModel: SharedViewModel, navCo
 @Composable
 fun ArticleItem(article: Article, sharedViewModel: SharedViewModel) {
     val date = article.publishedAt
-    val elapsedtime = calculateElapsedTime(date)
+    val elapsedTime = calculateElapsedTime(date)
+
     Row(
         modifier = Modifier
-            .fillMaxWidth()
-            .padding(16.dp),
+            .fillMaxWidth(),
         verticalAlignment = Alignment.CenterVertically
     ) {
         Box(
             modifier = Modifier
-                .width(100.dp)
-                .height(80.dp)
-                .clip(RoundedCornerShape(10.dp))
-                .padding(end = 10.dp)
+                .fillMaxWidth()
+                .height(200.dp)
         ) {
             AsyncImage(
                 model = ImageRequest.Builder(LocalContext.current)
-                    .data(article.urlToImage)
+                    .data(article.image)
                     .crossfade(true)
                     .build(),
                 contentDescription = article.title,
                 contentScale = ContentScale.Crop,
                 modifier = Modifier.fillMaxSize()
             )
-        }
-        Column(modifier = Modifier.weight(1f)) {
-            Text(
-                text = article.title,
-                fontWeight = FontWeight.Bold,
-                fontSize = 12.sp,
-                overflow = TextOverflow.Ellipsis,
-                maxLines = 1
-            )
-            article.description?.let {
-                Text(
-                    text = it,
-                    fontSize = 11.sp,
-                    maxLines = 2,
-                    overflow = TextOverflow.Ellipsis
+
+            // Use Chip composable for category and source
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .wrapContentHeight(), // Adjust height as needed
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                // Yellow background for the category
+                ChipSource(
+                    text = article.author ?: "Source",
+                    backgroundColor = Color.Yellow,
+                    textColor = Color.Black
+                )
+
+                Spacer(modifier = Modifier.width(2.dp)) // Spacing between the two parts
+
+                // Blue background for the source
+                ChipSource(
+                    text = article.source.name?: "Source",
+                    backgroundColor = Color.Blue,
+                    textColor = Color.White
                 )
             }
-            Text(
-                text = elapsedtime,
-                fontSize = 12.sp
-            )
+
+            Column(
+                modifier = Modifier
+                    .align(Alignment.BottomStart)
+                    .padding(8.dp)
+            ) {
+                Text(
+                    text = article.title,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 12.sp,
+                    overflow = TextOverflow.Ellipsis,
+                    maxLines = 2,
+                    color = Color.White
+                )
+                Text(
+                    text = elapsedTime,
+                    fontSize = 12.sp,
+                    color = Color.White
+                )
+            }
         }
+
         IconButton(
             onClick = {
                 sharedViewModel.toggleBookmark(article)
@@ -111,6 +136,28 @@ fun ArticleItem(article: Article, sharedViewModel: SharedViewModel) {
     }
 }
 
+@Composable
+fun ChipSource(
+    text: String,
+    backgroundColor: Color,
+    textColor: Color
+) {
+    Box(
+        modifier = Modifier
+            .wrapContentHeight()
+            .background(backgroundColor),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = text,
+            color = textColor,
+            fontSize = 12.sp,
+            fontWeight = FontWeight.Medium,
+            textAlign = TextAlign.Center
+        )
+    }
+}
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun BookmarkScreen(
@@ -118,14 +165,13 @@ fun BookmarkScreen(
     navController: NavController
 ) {
     val bookmarkedArticles = sharedViewModel.bookmarkedArticles.collectAsState().value
-
     Scaffold(
         topBar = {
             CenterAlignedTopAppBar(
                 title = {
                     Text(
                         stringResource(R.string.Favourites),
-                        fontWeight = FontWeight.Bold
+                        fontWeight = FontWeight.Normal
                     )
                 }
             )
