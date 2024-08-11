34a2d59c25825680563dd6bf3d52239488e2db22
mkaocodes <mkaoibrahim@gmail.com>
2024-08-11 20:58:03 +0300

    Refactor: Improve Article Animation
    - Introduced ArticleScreenState composable to manage state and events for the ArticleScreen.
    - Moved categories to ArticleScreenState for better
     organization.
    - Removed unnecessary LaunchedEffect and isLoading state.
    - Updated BottomSheet implementation to use SheetState from ArticleScreenState.
    - Added helper functions for hiding keyboard, clearing focus, and hiding bottom sheet.
    - Improved code readability and organization.

--- app/src/main/java/dev/mkao/weaver/presentation/home/ArticleScreen.kt
+++ app/src/main/java/dev/mkao/weaver/presentation/home/ArticleScreen.kt
@@ -1,6 +1,5 @@
 package dev.mkao.weaver.presentation.home
 
-import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -15,8 +14,8 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SheetState
 import androidx.compose.material3.Text
-import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -26,10 +25,12 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusManager
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.platform.SoftwareKeyboardController
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -47,153 +48,238 @@ import dev.mkao.weaver.presentation.common.BottomNavigationBar
 import dev.mkao.weaver.presentation.common.CardArtiCle
 import dev.mkao.weaver.presentation.common.TintedTextButton
 import dev.mkao.weaver.presentation.search.SearchAppBar
+import dev.mkao.weaver.viewModels.ArticleScreenViewModel
 import dev.mkao.weaver.viewModels.ArticleStates
 import dev.mkao.weaver.viewModels.SharedViewModel
-import kotlinx.coroutines.delay
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
-@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun ArticleScreen(
 	state: ArticleStates,
 	navController: NavController,
 	onReadFullStoryButtonClick: (Article) -> Unit,
-	onEvent: (EventsHolder) -> Unit
+	onEvent: (EventsHolder) -> Unit,
+	sharedViewModel: SharedViewModel = hiltViewModel()
 ) {
-	val coroutineScope = rememberCoroutineScope()
-	val categories = listOf("General", "Business", "Health", "Science", "Technology")
-	val focusManager = LocalFocusManager.current
-	val focusRequester = remember { FocusRequester() }
-	val keyboardController = LocalSoftwareKeyboardController.current
-	var shouldBottomSheetShow by remember { mutableStateOf(false) }
-	val sheetState = rememberModalBottomSheetState(skipPartiallyExpanded = false)
-	var isLoading by remember { mutableStateOf(true) }
-	val sharedViewModel: SharedViewModel = hiltViewModel()
+	val screenState = rememberArticleScreenState()
 
-	LaunchedEffect(true) {
-		onEvent(EventsHolder.OnCategoryClicked("General"))
-	}
-
-	if (shouldBottomSheetShow) {
-		ModalBottomSheet(
-			onDismissRequest = { shouldBottomSheetShow = false },
-			sheetState = sheetState,
-			content = {
-				state.isSelected?.let {
-					BottomDialog(
-						article = it,
-						onReadFullStoryButtonClicked = {
-							onReadFullStoryButtonClick(it)
-							coroutineScope.launch { sheetState.hide() }.invokeOnCompletion {
-								if (!sheetState.isVisible) shouldBottomSheetShow = false
-							}
-						}
-					)
-				}
-			}
-		)
+	LaunchedEffect(Unit) {
+		onEvent(EventsHolder.OnCategoryClicked("Technology"))
 	}
 
 	Scaffold(
-		topBar = {
-			Column(
-				modifier = Modifier
-					.fillMaxWidth()
-					.padding(top = 70.dp),
-				verticalArrangement = Arrangement.SpaceBetween,
-				horizontalAlignment = Alignment.Start
-			) {
-				Text(
-					modifier = Modifier.padding(horizontal = 10.dp),
-					text = stringResource(R.string.discover),
-					fontSize = 32.sp,
-					fontWeight = FontWeight.Bold,
-				)
-				Spacer(modifier = Modifier.height(15.dp))
-				Text(
-					modifier = Modifier.padding(horizontal = 10.dp),
-					text = stringResource(R.string.fresh_stories_and_bold_ideas_to_help_you_live_curiously),
-					fontSize = 16.sp,
-					fontWeight = FontWeight.Bold,
-				)
-			}
-		},
-		bottomBar = {
-			BottomNavigationBar(navController = navController)
-		},
+		topBar = { ArticleScreenTopBar() },
+		bottomBar = { BottomNavigationBar(navController = navController) },
 		content = { paddingValues ->
-			Column(
-				modifier = Modifier
-					.fillMaxSize()
-					.padding(paddingValues)
-			) {
-				SearchAppBar(
-					modifier = Modifier.focusRequester(focusRequester),
-					value = state.searchRequest,
-					onValueChange = { newQuery ->
-						onEvent(EventsHolder.OnSearchCategoryChanged(newQuery))
-					},
-					onCloseIconClicked = { onEvent(EventsHolder.OnCloseIconClicked) },
-					onSearchClicked = {
-						keyboardController?.hide()
-						focusManager.clearFocus()
-					}
-				)
-
-				Spacer(modifier = Modifier.height(2.dp))
-				LazyRow(
-					contentPadding = PaddingValues(horizontal = 2.dp),
-					horizontalArrangement = Arrangement.spacedBy(2.dp)
-				) {
-					items(categories.size) { index ->
-						val category = categories[index]
-						val isSelected = state.category == category
-						TintedTextButton(
-							isSelected = isSelected,
-							category = category,
-							onClick = {
-								sharedViewModel.selectCategory(category)
-								onEvent(EventsHolder.OnCategoryClicked(category)) }
-						)
-					}
-				}
-				if (state.isLoading) {
-					repeat(5) { index ->
-						ArticleCardShimmerEffect(
-							modifier = Modifier
-								.fillMaxWidth()
-								.padding(horizontal = 16.dp, vertical = 8.dp)
-						)
-					}
-				} else {
-					Spacer(modifier = Modifier.height(20.dp))
-					LazyColumn(
-						modifier = Modifier.fillMaxWidth(),
-						contentPadding = PaddingValues(2.dp),
-						verticalArrangement = Arrangement.spacedBy(2.dp)
-					) {
-						items(state.article) { article ->
-							CardArtiCle(
-								article = article,
-								onReadFullStoryClicked = {
-									shouldBottomSheetShow = true
-									onEvent(EventsHolder.OnArticleCardClicked(article))
-								}
-							)
-							Spacer(modifier = Modifier.height(0.5.dp))
-						}
-					}
-				}
-				LaunchedEffect(true) {
-					delay(4000)
-					isLoading = false
-				}
-			}
+			ArticleScreenContent(
+				state = state,
+				screenState = screenState,
+				paddingValues = paddingValues,
+				onEvent = onEvent,
+				sharedViewModel = sharedViewModel
+			)
 		}
+	)
+
+	if (screenState.shouldBottomSheetShow) {
+		ArticleBottomSheet(
+			article = state.isSelected,
+			sheetState = screenState.sheetState,
+			onDismiss = { screenState.shouldBottomSheetShow = false },
+			onReadFullStoryButtonClick = { article ->
+				onReadFullStoryButtonClick(article)
+				screenState.hideBottomSheet()
+			}
+		)
+	}
+}
+
+@Composable
+private fun ArticleScreenTopBar() {
+	Column(
+		modifier = Modifier
+			.fillMaxWidth()
+			.padding(top = 70.dp, start = 10.dp, end = 10.dp),
+		verticalArrangement = Arrangement.spacedBy(15.dp),
+		horizontalAlignment = Alignment.Start
+	) {
+		Text(
+			text = stringResource(R.string.discover),
+			fontSize = 32.sp,
+			fontWeight = FontWeight.Bold,
+		)
+		Text(
+			text = stringResource(R.string.fresh_stories_and_bold_ideas_to_help_you_live_curiously),
+			fontSize = 16.sp,
+			fontWeight = FontWeight.Bold,
+		)
+	}
+}
+
+@Composable
+private fun ArticleScreenContent(
+	state: ArticleStates,
+	screenState: ArticleScreenState,
+	paddingValues: PaddingValues,
+	onEvent: (EventsHolder) -> Unit,
+	sharedViewModel: SharedViewModel
+) {
+	Column(
+		modifier = Modifier
+			.fillMaxSize()
+			.padding(paddingValues)
+	) {
+		SearchAppBar(
+			modifier = Modifier.focusRequester(screenState.focusRequester),
+			value = state.searchRequest,
+			onValueChange = { newQuery -> onEvent(EventsHolder.OnSearchCategoryChanged(newQuery)) },
+			onCloseIconClicked = { onEvent(EventsHolder.OnCloseIconClicked) },
+			onSearchClicked = screenState::hideKeyboardAndClearFocus
+		)
+
+		Spacer(modifier = Modifier.height(2.dp))
+
+		CategorySelector(
+			categories = screenState.categories,
+			selectedCategory = state.category,
+			onEvent = onEvent // Pass the onEvent parameter here
+		)
+
+		if (state.isLoading) {
+			ArticleLoadingShimmer()
+		} else {
+			ArticleList(
+				articles = state.article,
+				onArticleClicked = { article ->
+					screenState.shouldBottomSheetShow = true
+					onEvent(EventsHolder.OnArticleCardClicked(article))
+				}
+			)
+		}
+	}
+}
+
+@Composable
+private fun CategorySelector(
+	categories: List<String>,
+	selectedCategory: String,
+	onEvent: (EventsHolder) -> Unit
+) {
+	val articleViewModel: ArticleScreenViewModel = hiltViewModel()
+	LazyRow(
+		contentPadding = PaddingValues(horizontal = 2.dp),
+		horizontalArrangement = Arrangement.spacedBy(2.dp)
+	) {
+		items(categories) { category ->
+			TintedTextButton(
+				isSelected = selectedCategory == category,
+				category = category,
+				onClick = {
+					articleViewModel.updateCategoryAndFetchArticles(category)
+					onEvent(EventsHolder.OnCategoryClicked(category))
+				}
+			)
+		}
+	}
+}
+
+@Composable
+private fun ArticleLoadingShimmer() {
+	repeat(5) {
+		ArticleCardShimmerEffect(
+			modifier = Modifier
+				.fillMaxWidth()
+				.padding(horizontal = 16.dp, vertical = 8.dp)
+		)
+	}
+}
+
+@Composable
+private fun ArticleList(
+	articles: List<Article>,
+	onArticleClicked: (Article) -> Unit
+) {
+	LazyColumn(
+		modifier = Modifier.fillMaxWidth(),
+		contentPadding = PaddingValues(2.dp),
+		verticalArrangement = Arrangement.spacedBy(2.dp)
+	) {
+		items(articles) { article ->
+			CardArtiCle(
+				article = article,
+				onReadFullStoryClicked = { onArticleClicked(article) }
+			)
+			Spacer(modifier = Modifier.height(0.5.dp))
+		}
+	}
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun ArticleBottomSheet(
+	article: Article?,
+	sheetState: SheetState,
+	onDismiss: () -> Unit,
+	onReadFullStoryButtonClick: (Article) -> Unit
+) {
+	ModalBottomSheet(
+		onDismissRequest = onDismiss,
+		sheetState = sheetState,
+		dragHandle = null
+	) {
+		article?.let {
+			BottomDialog(
+				article = it,
+				onReadFullStoryButtonClicked = { onReadFullStoryButtonClick(it) }
+			)
+		}
+	}
+}
+
+@Composable
+fun rememberArticleScreenState(
+	categories: List<String> = listOf("General", "World", "Nation", "Business", "Technology","Science","Health"),
+	coroutineScope: CoroutineScope = rememberCoroutineScope(),
+	focusManager: FocusManager = LocalFocusManager.current,
+	focusRequester: FocusRequester = remember { FocusRequester() },
+	keyboardController: SoftwareKeyboardController? = LocalSoftwareKeyboardController.current
+) = remember {
+	ArticleScreenState(
+		categories = categories,
+		coroutineScope = coroutineScope,
+		focusManager = focusManager,
+		focusRequester = focusRequester,
+		keyboardController = keyboardController
 	)
 }
 
+class ArticleScreenState(
+	val categories: List<String>,
+	private val coroutineScope: CoroutineScope,
+	private val focusManager: FocusManager,
+	val focusRequester: FocusRequester,
+	private val keyboardController: SoftwareKeyboardController?
+) {
+	var shouldBottomSheetShow by mutableStateOf(false)
+	@OptIn(ExperimentalMaterial3Api::class)
+	val sheetState = SheetState(skipPartiallyExpanded = false, density = LocalDensity.current)
+
+	fun hideKeyboardAndClearFocus() {
+		keyboardController?.hide()
+		focusManager.clearFocus()
+	}
+
+	@OptIn(ExperimentalMaterial3Api::class)
+	fun hideBottomSheet() {
+		coroutineScope.launch {
+			sheetState.hide()
+		}.invokeOnCompletion {
+			if (!sheetState.isVisible) shouldBottomSheetShow = false
+		}
+	}
+}
 
 
 @Preview(showBackground = true)
